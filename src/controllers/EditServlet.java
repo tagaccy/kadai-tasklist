839e86211e79355de83cf4b355760cf4d8784e30
yuko.tagashira <yuko.tagashira@axas-japan.co.jp>
2022-02-06 22:51:21 +0900

    課題提出

--- src/controllers/EditServlet.java
+++ src/controllers/EditServlet.java
@@ -2,6 +2,7 @@ package controllers;
 
 import java.io.IOException;
 
+import javax.persistence.EntityManager;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,18 +11,19 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import models.Tasks;
+import util.DBUtil;
 
 /**
- * Servlet implementation class NewServlet
+ * Servlet implementation class EditServlet
  */
-@WebServlet("/new")
-public class NewServlet extends HttpServlet {
+@WebServlet("/edit")
+public class EditServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public NewServlet() {
+    public EditServlet() {
         super();
     }
 
@@ -29,13 +31,21 @@ public class NewServlet extends HttpServlet {
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // CSRF対策
+        EntityManager em = DBUtil.createEntityManager();
+
+        // 該当のIDのメッセージ1件のみをデータベースから取得
+        Tasks m = em.find(Tasks.class, Integer.parseInt(request.getParameter("id")));
+
+        em.close();
+
+        // メッセージ情報とセッションIDをリクエストスコープに登録
+        request.setAttribute("tasks", m);
         request.setAttribute("_token", request.getSession().getId());
 
-        // おまじないとしてのインスタンスを生成
-        request.setAttribute("message", new Tasks());
+        // メッセージIDをセッションスコープに登録
+        request.getSession().setAttribute("tasks_id", m.getId());
 
-        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/new.jsp");
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/edit.jsp");
         rd.forward(request, response);
-     }
+    }
 }
